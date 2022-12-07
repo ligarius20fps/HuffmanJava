d293f55954aeaecada1f1a94468928ad510ead5d
ligarius20fps <ligarius20fps@users.noreply.github.com>
2022-12-07 16:56:56 +0100

    w środku pracy

---
+++ src/Main.java
@@ -0,0 +1,46 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args)
+    {
+        if(args.length == 0)
+        {
+            System.out.println("Please provide a file name");
+            System.exit(1);
+        }
+        String strFile = args[0];
+        //odczytać plik
+        try (FileInputStream in = new FileInputStream(strFile)) {
+            int c;
+            //key: byte
+            //value: number of same bytes
+            Map<Integer, Integer> map = new HashMap<>();
+            while ((c = in.read()) != -1)
+            {
+                //policzyć liczbę poszczególnych bajtów
+                if(map.containsKey(c)){
+                    int numberOfOccurences = map.get(c);
+                    map.put(c,++numberOfOccurences);
+                }
+                else{
+                    map.put(c,1);
+                }
+            }
+            System.out.println("good");
+        } catch (FileNotFoundException e) {
+            System.out.println("Could not find file: " + strFile);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println("hello");
+
+        //można by rozwinąć o kolejne n bajtów
+        //zbudować drzewo
+        //z drzewa w ciąg bitów
+        //zapisać w postaci skompresowanej
+    }
+}
